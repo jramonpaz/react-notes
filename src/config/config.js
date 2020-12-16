36b293b219f20711cb9f2627caa9e4cdd288314c
jramonpaz <jramonpaz@gmail.com>
2020-12-16 22:45:27 +0100

    Primer commit

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,9 @@
 // Initialize Firebase
 export const DB_CONFIG = {
-	apiKey: "AIzaSyCKEvUfM4eFXzRePpaMjbPVQGVWnk8z9Ac",
-	authDomain: "reactnotes-a205e.firebaseapp.com",
-	databaseURL: "https://reactnotes-a205e.firebaseio.com",
-	projectId: "reactnotes-a205e",
-	storageBucket: "",
-	messagingSenderId: "904756603733"
+	apiKey: "AIzaSyC8jtsNMVeWbfYKBpR2fjFBq9JAb08AKKI",
+    authDomain: "reactnotes-be324.firebaseapp.com",
+    projectId: "reactnotes-be324",
+    storageBucket: "reactnotes-be324.appspot.com",
+    messagingSenderId: "429952292183",
+    appId: "1:429952292183:web:09b44f4f12a8315fb91a12"
 };
