749ecf25ea77239ed7be39fe15808952e74318d8
jramonpaz <jramonpaz@gmail.com>
2020-12-17 21:10:30 +0100

    Funcionando

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,10 @@
 // Initialize Firebase
 export const DB_CONFIG = {
-	apiKey: "AIzaSyC8jtsNMVeWbfYKBpR2fjFBq9JAb08AKKI",
-    authDomain: "reactnotes-be324.firebaseapp.com",
-    projectId: "reactnotes-be324",
-    storageBucket: "reactnotes-be324.appspot.com",
-    messagingSenderId: "429952292183",
-    appId: "1:429952292183:web:09b44f4f12a8315fb91a12"
+  apiKey: "AIzaSyC8jtsNMVeWbfYKBpR2fjFBq9JAb08AKKI",
+  authDomain: "reactnotes-be324.firebaseapp.com",
+  projectId: "reactnotes-be324",
+  storageBucket: "reactnotes-be324.appspot.com",
+  messagingSenderId: "429952292183",
+  appId: "1:429952292183:web:09b44f4f12a8315fb91a12",
+  databaseURL: "https://react-notes-d38fa-default-rtdb.firebaseio.com",
 };
